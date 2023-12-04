5fae1fec347cb4777b1063d7163aeeb34ec1b9b5
Udari Rathnasiri <udarihashini@gmail.com>
2023-12-04 18:14:24 +0530

    Implement createNewLecturer database part

--- src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
@@ -1,9 +1,25 @@
 package lk.ijse.dep11.edupanel;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class WebRootConfig {
+    @Bean
+    public HikariDataSource dataSource(Environment env){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        config.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        config.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        config.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        config.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari.maximum-pool-size",Integer.class));
+        return new HikariDataSource(config);
+    }
 }
